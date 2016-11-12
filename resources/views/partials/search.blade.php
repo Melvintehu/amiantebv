d2f7b1e7de77a05a95f8c330f2645886be5c2bae
Elandlord <e.landheer@mentechmedia.nl>
2016-11-12 16:54:27 +0100

    Search bar

---
+++ resources/views/partials/search.blade.php
@@ -0,0 +1,40 @@
+<div class='container space-outside-sm hidden-xs'>
+
+    <div class="row">    
+            
+        <div class="col-lg-4 float-right">
+
+            <form> 
+
+                <div class="form-group">
+
+                    <div class="col-lg-7 col-md-7 col-sm-7">
+
+                        <input type="text" class="form-control 
+                                       border-hover-main 
+                                       border-focus-main
+                                       space-outside-up-xs
+                                        " 
+                        id="searchbar-home" placeholder="Zoeken.." name="q"> 
+
+                    </div>
+
+                    <div class="col-lg-5 col-md-5 col-sm-5">
+
+                        <button type="button" class="btn-standard bg-main text-color-light space-inside-sides-md">
+
+                            <i class="glyphicon glyphicon-search"></i>
+
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </form>
+
+        </div>
+
+    </div>
+
+</div>
