516b39f4fe4199c73e16f2867a01f730b9a73dea
Eric Landheer <Eric Landheer>
2016-12-12 18:47:30 +0100

    Diensten
    dienstenpagina

--- resources/views/pages/diensten.blade.php
+++ resources/views/pages/diensten.blade.php
@@ -13,27 +13,36 @@
 					
 					<h1 class="text-thin space-inside-down-sm">Onze <span class="text-bold">Diensten</span></h1>
 					
-					<p class="text-color-accent-darken-md font-md"> Hieronder vind je onze diensten</p>
+					<p class="text-color-accent-darken-md font-md xs-font-sm"> Hieronder vind je onze diensten</p>
 
 				</div>
 
 
 			<div class="col-lg-6 space-inside-sides-xl xs-space-outside-up-md">
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac fermentum arcu. Quisque tortor dui, imperdiet quis lectus sit amet, vestibulum hendrerit nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus tempus in leo et molestie. Sed lorem est, placerat id tincidunt vitae, molestie in ex. Cras ullamcorper finibus auctor. Pellentesque leo velit, tempor ut felis et, tristique facilisis arcu.
+				<p class='font-md xs-font-sm'>
+					Voor zowel bedrijven, particulieren en overheidsinstanties verzorgen wij het gehele asbest en/of sloopproject. U kunt ook bij ons terecht voor advisering en begeleiding voor het gehele traject. Vooraf wordt met u besproken wat u wilt laten verzorgen, waarna er vervolgens een transparant kostenoverzicht voor u wordt opgesteld. Een groot voordeel hiervan is dat u één aanspreekpunt heeft voor al uw asbest gerelateerde werkzaamheden. 
 				</p>
 				<br/>
-				<p>
-					Donec tristique, purus at consequat molestie, nibh justo vulputate odio, eu accumsan mauris diam fringilla massa. Donec in sodales turpis, nec laoreet orci. Pellentesque nec turpis mi. Ut quis tortor vitae nunc egestas posuere vitae sit amet purus. Phasellus facilisis varius ante eget porta. Nam faucibus mauris eget luctus hendrerit.
-				</p>
 			</div>
 
 
-			<div class="col-lg-6 space-inside-sides-xl xs-space-outside-up-md">
-
-
-				<img src='images/checklist.png' class=''/>
-
+			<div class="col-lg-6 space-inside-sides-xl font-md xs-font-sm">
+				<h2 class='xs-font-md'>Werkzaamheden waarin wij u kunnen adviseren & begeleiden: </h2>
+				<br/>
+				<ul class=''>
+					<li>Uitvoering asbestinventarisaties, type A, type B, type 0 </li>
+					<li>Aanvraag sloopvergunning </li>
+					<li>Opstellen kostenraming van de werkzaamheden </li>
+					<li>Opstellen van een bestek </li>
+					<li>Offerte aanvraag voor werkzaamheden </li>
+					<li>Aanschrijven van geselecteerde asbest sloopbedrijven </li>
+					<li>Vergelijking van offertes en/of inschrijvingen </li>
+					<li>Het gunnen van de opdracht </li>
+					<li>Controle op de uitvoering van de werkzaamheden </li>
+					<li>Verzorgen van de eindcontrole bij asbest werkzaamheden </li>
+					<li>Afhandeling van correspondentie van afloop van de werkzaamheden </li>
+					<li>Detachering </li>
+				</ul>
 			</div>
 
 			<!-- <div class="col-lg-6 space-inside-sides-xl">
@@ -59,6 +68,149 @@
 
 
 			</div> -->
+
+		</div>
+
+	</section>
+
+	<section class="container space-inside-down-lg animated fadeInDown wow">
+
+		<div class="row all-row-centered">
+
+		<div class="col-lg-12 space-inside-down-lg text-center">
+					
+					<h1 class="text-thin space-inside-down-sm space-outside-up-md xs-font-lg">Types <span class="text-bold">asbestinventarisaties</span></h1>
+					
+					<p class="text-color-accent-darken-md font-md xs-font-sm"> Hieronder volgt de uitleg van de verschillende asbestinventarisaties</p>
+
+				</div>
+
+
+		<!-- blok 1 -->
+			<div class="
+						col-lg-4
+						col-md-4
+						col-sm-12
+						sm-space-outside-down-md
+						col-xs-12 
+						xs-space-outside-down-md
+						xs-max-width-lg		
+						 
+			">
+
+					<div class="col-lg-12
+								lg-min-height-sm
+								col-md-12
+								col-sm-12
+								sm-space-inside-md
+								col-xs-12
+								xs-text-center
+								bg-accent 
+								space-inside-sm 
+								space-inside-sides-md
+					">
+						
+						<h5 class="space-outside-down-sm ">Type A</h5>
+
+						<p>
+							Voorafgaand aan sloop of renovatie bestaat de wettelijke verplichting een volledige asbestinventarisatie TYPE A te laten uitvoeren door een SC-540 gecertificeerd asbestinventarisatiebureau. In de rapportage asbestinventarisatie TYPE A zijn alle direct waarneembare asbesthoudende toepassingen opgenomen zodat u een compleet beeld heeft van de aanwezige (visueel waarneembare) asbesthoudende toepassingen.
+						</p><br/>
+						<p>
+							Tijdens een asbestinventarisatie TYPE A worden geen zwaar destructieve onderzoekshandelingen uitgevoerd. Asbesthoudende toepassingen die alleen met behulp van zwaar destructieve onderzoek kunnen worden waargenomen dienen middels een asbestinventarisatie TYPE B inzichtelijk te worden gemaakt.
+						</p><br>
+						<p>
+							Met een rapportage asbestinventarisatie TYPE A kan een melding asbestverwijdering of in geval van sloop een sloopmelding worden gedaan bij gemeente zodat de asbest verwijderd kan worden door een SC-530 gecertificeerd asbest verwijderingsbedrijf.
+						</p>
+
+
+					</div>
+
+			</div>
+
+
+			<!-- blok 2 -->
+			<div class="
+						col-lg-4 
+						col-md-4
+						col-sm-12
+						sm-space-outside-down-md
+						col-xs-12
+						xs-space-outside-down-md
+						xs-max-width-lg	 	 
+			">
+
+					<div  class="col-lg-12
+								 lg-min-height-sm
+								 col-md-12
+								 col-sm-12
+								 sm-space-inside-md
+								 col-xs-12
+								 xs-text-center
+								 bg-accent 
+								 space-inside-sm 
+								 space-inside-sides-md
+					">
+						
+						<h5 class="space-outside-down-sm">Type B</h5>
+
+						<p>
+							Tijdens een asbestinventarisatie Type A worden de visueel direct waarneembare asbesthoudende toepassingen in kaart gebracht. Veelal wordt een asbestinventarisatie Type A in een bewoonde situatie uitgevoerd en worden er derhalve geen destructieve handelingen ten uitvoer gebracht. Dit om de integriteit en de functionaliteit van een gebouw niet aan te tasten.
+						</p><br/>
+						<p>
+							Wanneer tijdens een asbestinventarisatie Type A locaties worden waargenomen waarvan het vermoeden bestaat dat hier asbesthoudende toepassingen aanwezig kunnen zijn die alleen met zwaar destructief onderzoek inzichtelijk kunnen worden gemaakt (z.g. uitsluitingen), dient voorafgaand aan sloop of renovatie een aanvullende Type B inventarisatie te worden uitgevoerd.
+						</p><br/>
+						<p>
+							In onze rapportages asbestinventarisatie Type A worden deze uitsluitingen letterlijk aangegeven, zodat voorafgaand aan sloop of renovatie duidelijk is welke delen van een gebouw nog aanvullend onderzocht dienen te worden. Indien er locaties aanwezig zijn die niet onderzocht zijn maar waar geen vermoeden van bestaat dat hier zich asbesthoudende toepassingen bevinden, hoeven deze locaties niet aanvullend te worden onderzocht. De rapportage asbestinventarisatie Type B dient net als de rapportage van asbestinventarisatie Type A te worden toegevoegd aan het omgevingsloket.
+
+						</p>
+
+
+				</div>
+
+			</div>
+
+
+			<!-- blok 3 -->
+			<div  class="
+						 col-lg-4
+						 col-md-4
+						 col-sm-12 
+						 col-xs-12
+						 xs-max-width-lg
+
+			">
+
+					<div  class="col-lg-12
+								 lg-min-height-sm
+								 col-md-12
+								 col-sm-12
+								 sm-space-inside-md
+								 col-xs-12
+								 xs-text-center
+								 bg-accent 
+								 space-inside-sm 
+								 space-inside-sides-md
+					">
+						
+						<h5 class="space-outside-down-sm xs-text-center">Type 0</h5>
+
+						<p>
+							Type 0 inventarisatie is bedoeld bij de ver-/aankoop van een pand of woning. Tijdens deze inventarisatie wordt geen destructief onderzoek uitgevoerd, tevens worden er geen materiaalmonsters genomen van de aangetroffen asbestverdachte materialen.
+						</p>
+						<p>
+							Het betreft enkel een visuele asbestbeoordeling op de aanwezigheid van asbestverdachte materialen. Door middel van de rapportage kan er een kostenraming worden opgesteld welke gebruikt kan worden bij een eventuele overdracht.
+						</p>
+						<p>
+							Een type 0 inventarisatie is niet geschikt voor het aanvragen van een sloopmelding of omgevingsvergunning, hiervoor dient een type A inventarisatie uitgevoerd te worden.
+
+						</p>
+
+					</div>
+
+
+			</div>
+
+
 
 		</div>
 
